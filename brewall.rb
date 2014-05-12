e75d40bb45e9c3fe81efba1155f1298a59d341e2
rcmdnk <rcmdnk@gmail.com>
2014-05-13 02:15:41 +0900

    updated to v0.1.0

--- brewall.rb
+++ brewall.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-HOMEBREW_BREWALL_VERSION = '0.0.9'
+HOMEBREW_BREWALL_VERSION = '0.1.0'
 class Brewall < Formula
   homepage 'https://github.com/rcmdnk/homebrew-brewall/'
   url 'https://github.com/rcmdnk/homebrew-brewall.git', :tag => "v#{HOMEBREW_BREWALL_VERSION}"
