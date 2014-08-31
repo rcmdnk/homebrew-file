d9ba30f0e7f06bffcb64be3001f2d8c0b40c8242
rcmdnk <rcmdnk@gmail.com>
2014-09-01 08:59:49 +0900

    added python version in formula

--- brew-file.rb
+++ brew-file.rb
@@ -6,6 +6,15 @@ class BrewFile < Formula
   url 'https://github.com/rcmdnk/homebrew-file.git', :tag => "v#{HOMEBREW_BREW_FILE_VERSION}"
   version HOMEBREW_BREW_FILE_VERSION
   head 'https://github.com/rcmdnk/homebrew-file.git', :branch => 'master'
+  devel do
+    url 'https://github.com/rcmdnk/homebrew-file.git', :branch => 'python'
+    #if build.include? "ruby"
+    #  url 'https://github.com/rcmdnk/homebrew-file.git', :branch => 'ruby'
+    #end
+  end
+
+  option "python", "Use python version (default for devel version)"
+  #option "ruby", "Use ruby version"
 
   skip_clean 'bin'
 
