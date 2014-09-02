8ce9ae7c495aaf67c26f9360f966530566199e94
rcmdnk <rcmdnk@gmail.com>
2014-09-03 03:57:40 +0900

    test

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 brew file version
 brew file help
 brew install vim --with-lua --HEAD
-brew install caskroom/cask/brew-file
+brew install caskroom/cask/brew-cask
 brew cask install firefox
 brew file init
 brew file update
