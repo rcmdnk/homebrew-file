4de1366cd59677786643fd9563d472a6031383d1
rcmdnk <rcmdnk@gmail.com>
2014-09-03 04:23:26 +0900

    a

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ brew file help
 brew install vim --with-lua --HEAD
 brew install caskroom/cask/brew-cask
 brew cask install firefox
-brew file init
-brew file update
-brew file casklist
+#brew file init
+#brew file update
+#brew file casklist
 #brew file edit
