dd731fd6437c7da84f2941a2f4e533eabfeb7b36
rcmdnk <rcmdnk@gmail.com>
2014-09-03 04:56:52 +0900

    test

--- test.sh
+++ test.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
-echo brew install rcmdnk/file/brew-file $version
-brew install rcmdnk/file/brew-file $version
-echo brew file version
-brew file version
-echo brew file help
-brew file help
-echo brew install vim --with-lua --HEAD
-brew install vim --with-lua --HEAD
-echo brew install caskroom/cask/brew-cask
-brew install caskroom/cask/brew-cask
-echo brew cask install firefox
-brew cask install firefox
+f (){
+  echo $*
+  $*
+}
+f brew install rcmdnk/file/brew-file $version
+f brew file version
+f brew file help
+f brew install vim --with-lua --HEAD
+f brew install caskroom/cask/brew-cask
+f brew cask install firefox
 #brew file init
 #brew file update
 #brew file casklist
