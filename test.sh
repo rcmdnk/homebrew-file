a128e821b819dc21acb9712678cde8958817ce0c
rcmdnk <rcmdnk@gmail.com>
2014-09-03 13:50:56 +0900

    test

--- test.sh
+++ test.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
+set -e
 f (){
-  echo $*
+  echo \$ $*
   $*
 }
+f export PAGER=cat
+f export EDITOR=cat
+f export VISUAL=cat
 f brew install rcmdnk/file/brew-file $version
 f brew file version
 f brew file help
 f brew install vim --with-lua --HEAD
 f brew install caskroom/cask/brew-cask
 f brew cask install firefox
-#brew file init
-#brew file update
-#brew file casklist
-#brew file edit
+f brew file init
+f brew file update
+f brew file casklist
+f brew file edit
