1ed0b2bb2586a055ceee2a739602b0106af65263
rcmdnk <rcmdnk@gmail.com>
2014-11-11 01:44:08 +0900

    stay Brewfile repository to brew repository

--- brew-file.rb
+++ brew-file.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-HOMEBREW_BREW_FILE_VERSION = '2.1.4'
+HOMEBREW_BREW_FILE_VERSION = '2.1.5'
 HOMEBREW_BREW_FILE_BASH_VERSION = '1.1.8'
 class BrewFile < Formula
   homepage 'https://github.com/rcmdnk/homebrew-file/'
