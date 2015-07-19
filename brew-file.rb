224c89d3897c4e19645daac6a027d88991fb4972
rcmdnk <rcmdnk@gmail.com>
2015-07-20 00:04:56 +0900

    updated formula

--- brew-file.rb
+++ brew-file.rb
@@ -1,25 +1,17 @@
 class BrewFile < Formula
   desc "Brewfile manager for Homebrew."
   homepage "https://github.com/rcmdnk/homebrew-file/"
-  url "https://github.com/rcmdnk/homebrew-file.git",
-    :tag => "v3.5.9",
-    :revision => "575574b9e6d270dc2f3955df0add6c03ed18b663"
-  head "https://github.com/rcmdnk/homebrew-file.git", :branch => "master"
-  if build.with? "bash"
-    url "https://github.com/rcmdnk/homebrew-file.git", :branch => "bash"
-    version "1.1.8"
-  end
+  url "https://github.com/rcmdnk/homebrew-file/archive/v3.5.9.tar.gz"
+  sha256 "2c7028a3798fcbf5569c5e318edb34bb48f79ff3765f10beb54dcaf385863bf2"
 
-  option "with-python", "Use python version (same as default)"
-  option "with-bash", "Use bash version"
+  head "https://github.com/rcmdnk/homebrew-file.git"
+
   option "without-completions", "Disable bash/zsh completions"
-
-  skip_clean "bin"
 
   def install
     bin.install "bin/brew-file"
     (bin+"brew-file").chmod 0755
-    (prefix+"etc").install "etc/brew-wrap"
+    etc.install "etc/brew-wrap"
     if build.with? "completions"
       bash_completion.install "etc/bash_completion.d/brew-file"
       zsh_completion.install "share/zsh/site-functions/_brew-file"
