881c3f269194f3e0ac8bb159f48de223ebda64e5
rcmdnk <rcmdnk@gmail.com>
2015-09-07 12:44:45 +0900

    fixed typo

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 brew_installed=1
 if ! which brew >& /dev/null;then
-  brwe_installed=0
+  brew_installed=0
   echo Homebrew is not installed!
   echo Install now...
   echo ruby -e \"\$\(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install\)\"
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   echo
 fi
+
 echo
 echo Install Brew-file...
-brew tap rcmdnk/file
-brew install brew-file
+brew install rcmdnk/file/brew-file
 
 if [ $brew_installed -eq 0 ];then
   brew doctor
