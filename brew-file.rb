7f3d254b6c0fb443aa3bf27e5aad0b8c1590fed6
rcmdnk <rcmdnk@gmail.com>
2015-12-18 15:56:55 +0900

    use cmd

--- brew-file.rb
+++ brew-file.rb
@@ -1,21 +1,17 @@
 class BrewFile < Formula
   desc "Brewfile manager for Homebrew."
   homepage "https://github.com/rcmdnk/homebrew-file/"
-  url "https://github.com/rcmdnk/homebrew-file/archive/v3.7.6.tar.gz"
-  sha256 "3f720c1777fab6fb3e96bc8db902779a3d730c0433d3df8d0fc6c8bb65260514"
-
-  head "https://github.com/rcmdnk/homebrew-file.git"
-
-  option "without-completions", "Disable bash/zsh completions"
+  url "https://github.com/rcmdnk/homebrew-file"
 
   def install
-    bin.install "bin/brew-file"
-    (bin+"brew-file").chmod 0755
-    etc.install "etc/brew-wrap"
-    if build.with? "completions"
-      bash_completion.install "etc/bash_completion.d/brew-file"
-      zsh_completion.install "share/zsh/site-functions/_brew-file"
-    end
+  end
+
+  def caveats
+    <<-EOS.undent
+      Homebrew-file is installed by `brew tap rcmdnk/homebrew-file`.
+      and is updated by `brew update`.
+      `brew install` is no longer needed.
+    EOS
   end
 
   test do
