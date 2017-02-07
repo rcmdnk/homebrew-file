ceb56fb51349f71d1e89fff59c67fec2c2421daa
rcmdnk <rcmdnk@gmail.com>
2017-02-07 15:15:41 +0900

    formula update

--- brew-file.rb
+++ brew-file.rb
@@ -1,8 +1,8 @@
 class BrewFile < Formula
   desc "Brewfile manager for Homebrew."
   homepage "https://github.com/rcmdnk/homebrew-file/"
-  url "https://github.com/rcmdnk/homebrew-file/archive/v4.1.7.tar.gz"
-  sha256 "b1abb94aa5eaaa44d11d4ef5afafe266b410e7bc1fcddbf4604f8bed3e8a7b59"
+  url "https://github.com/rcmdnk/homebrew-file/archive/v4.1.8.tar.gz"
+  sha256 "d5d23ef3e8de8906ad579a45a89f31a3c0e1ed79c9b5cc59b9cd560436e0ba02"
 
   head "https://github.com/rcmdnk/homebrew-file.git"
 
