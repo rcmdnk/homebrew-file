c8ad0b4ab288e6d31cee2788c2b79bad32ffb179
rcmdnk <rcmdnk@gmail.com>
2023-01-05 15:15:29 +0900

    write out brew-file

--- combine.sh
+++ combine.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-output=my_script.py
+output=./bin/brew-file
 echo "#!/usr/bin/env python3" > $output
 grep -E "(^from|^import)" src/brew_file/*.py|grep -v "from \."| cut -d ":" -f2|sort -u >> $output
 cat src/brew_file/info.py|grep -vE "(^from|^import)" >> $output
