28d6d42e29a244824c76bdd9d60696002d0232fb
rcmdnk <rcmdnk@gmail.com>
2023-02-01 09:49:42 +0900

    show diff of combined script

--- combine.sh
+++ combine.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 
 
-output=./bin/brew-file
+dest="./bin/brew-file"
+tmp_backup=$(mktemp -t brew-file)
+mv "$dest" "$tmp_backup"
+echo "Old brew-file was moved to $tmp_backup"
+
 {
   echo "#!/usr/bin/env python3"
-  grep -E "(^from|^import)" src/brew_file/*.py|grep -v "from \."| cut -d ":" -f2|sort -u
+  grep -E "(^from|^import)" src/brew_file/*.py|grep -v "from \."| cut -d ":" -f2| sort -u
   grep -vE "(^from|^import)" < src/brew_file/info.py
   grep -vE "(^from|^import)" < src/brew_file/utils.py
   grep -vE "(^from|^import)" < src/brew_file/brew_helper.py
   grep -vE "(^from|^import)" < src/brew_file/brew_info.py
   grep -vE "(^from|^import)" < src/brew_file/brew_file.py
   grep -vE "(^from|^import)" < src/brew_file/main.py
-} > $output
+} > "$dest"
 
-black $output
-isort $output
-autoflake --in-place $output
-autopep8 --in-place $output
+black "$dest"
+isort "$dest"
+autoflake --in-place "$dest"
+autopep8 --in-place "$dest"
+
+diff -u "$tmp_backup" "$dest"
+chmod 755 "$dest"
