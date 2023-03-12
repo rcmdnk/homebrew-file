a8b5dcf1cf8a2b8147a51c0c3d86cb34c8310ca9
rcmdnk <rcmdnk@gmail.com>
2023-03-12 10:30:07 +0900

    chore: update formula to v9.0.7

--- brew-file.rb
+++ brew-file.rb
@@ -1,8 +1,8 @@
 class BrewFile < Formula
   desc "Brewfile manager for Homebrew."
   homepage "https://github.com/rcmdnk/homebrew-file/"
-  url "https://github.com/rcmdnk/homebrew-file/archive/v9.0.6.tar.gz"
-  sha256 "a921d708926700bc1d985296d482104a412b5f59f640adb58f398233fa327d7e"
+  url "https://github.com/rcmdnk/homebrew-file/archive/v9.0.7.tar.gz"
+  sha256 "76fb0878ecd5f848a2ce29d8769ebc035fd56594aae4a5be0789aca93ceae108"
 
   head "https://github.com/rcmdnk/homebrew-file.git"
 
