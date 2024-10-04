cf9fd8172625be2e397d491e43b3bd6eeddbd781
rcmdnk <rcmdnk@gmail.com>
2024-10-05 01:42:14 +0900

    refactor: convert exe variables to arrays for better compatibility fix: prevent command execution when brew-file is missing refactor: optimize cask conditional logic fix: correct argument matching for specialized brew commands refactor: use subcommands with arrays to ensure correct execution fix: simplify brewfile retrieval with error suppression refactor: optimize mas command execution with array handling refactor: add whalebrew command wrapper with brew-file integration refactor: add code command wrapper for VSCode with brew-file support refactor: move from eval execution to direct exe call in fish scripts fix: ensure noinit option is properly handled for fish script refactor: add fish command wrappers for mas, whalebrew, and code commands refactor: optimize argument parsing in fish script

--- etc/brew-wrap.fish
+++ etc/brew-wrap.fish
@@ -1,48 +1,129 @@
-#!/usr/bin/env fish
 # brew command wrapper for brew-file
-
 function brew
-  set brew_exe (which brew)
-
   # Set exe
-  set exe "$brew_exe"
+  set -l exe command brew
+
+  if not type -q brew-file
+    $exe $argv
+    return
+  end
 
   # Do wrap
   # Check args
-  set nargs (count $argv)
-  set cmd $argv[1]
-  set cmd2 $argv[2]
+  set -l nargs 0
+  for a in $argv
+    if string match -qv '^-' -- $a
+      set nargs (math $nargs + 1)
+    end
+  end
+  set -l cmd $argv[1]
+  set -l cmd2 $argv[2]
 
   # No auto update for tap listing
-  set env ""
-  if test "$cmd" = "tap" && test $nargs -eq 1
+  set -l env ""
+  if test "$cmd" = "tap" -a $nargs -eq 1
     set env "HOMEBREW_NO_AUTO_UPDATE=1"
   end
 
-  switch $cmd
-    # brew file
-    case file
-      set exe "brew-file"
-    # brew install/uninstall/tap
-    case install reinstall tap rm remove uninstall untap
-      if [ $nargs -gt 1 ]
-        set exe "brew-file brew"
+  switch "$cmd"
+    case 'file'
+      set exe brew-file
+      set argv $argv[2..-1]
+    case 'install' 'reinstall' 'tap' 'rm' 'remove' 'uninstall' 'untap'
+      if test $nargs -gt 1
+        set exe brew-file brew
       end
-    # brew cask install/uninstall
-    case cask
-      if [ $nargs -gt 2 ]
-        if [ "$cmd2" = "rm" ] || [ "$cmd2" = "remove" ] || \
-            [ "$cmd2" = "uninstall" ] || [ "$cmd2" = "install" ] || \
-            [ "$cmd2" = "instal" ]
-          set exe "brew-file brew"
+    case 'cask'
+      if test $nargs -gt 2
+        switch "$cmd2"
+          case 'rm' 'remove' 'uninstall' 'install' 'instal'
+            set exe brew-file brew
         end
       end
     case '*'
-      if echo " brew init dump set_repo set_local pull push clean clean_non_request casklist get_files version "|grep -q -- " $argv[1] "
-        set exe "brew-file"
+      if echo " brew init dump set_repo set_local pull push clean clean_non_request casklist get_files version "| grep -q " $cmd "
+        set exe brew-file
       end
   end
 
-  eval $env $exe $argv
-  return $status
+  # Remove noinit option for pure brew command
+  if test "$exe" = "command brew" -a "$argv[1]" = "noinit"
+    set argv $argv[2..-1]
+  end
+
+  # Execute command
+  set -l brewfile (brew-file cat 2>/dev/null)
+  eval "$env $exe $argv"
+  set -l ret $status
+
+  # Commands after brew command
+  set -l brewfile_after (brew-file cat 2>/dev/null)
+  if test $ret -eq 0
+    if not diff (echo $brewfile | psub) (echo $brewfile_after | psub) >/dev/null
+      _post_brewfile_update
+    end
+  end
+
+  return $ret
+end
+
+function _post_brewfile_update
+  # Empty function placeholder
+end
+
+# mas command wrapper for brew-file
+function mas
+  # Set exe
+  set -l exe command mas
+
+  if type -q brew-file
+    # Check args
+    set -l cmd $argv[1]
+
+    switch "$cmd"
+      case 'purchase' 'install' 'uninstall'
+        set exe brew-file brew mas
+    end
+  end
+
+  # Execute command
+  $exe $argv
+end
+
+# whalebrew command wrapper for brew-file
+function whalebrew
+  # Set exe
+  set -l exe command whalebrew
+
+  if type -q brew-file
+    # Check args
+    set -l cmd $argv[1]
+
+    switch "$cmd"
+      case 'install' 'uninstall'
+        set exe brew-file brew whalebrew
+    end
+  end
+
+  # Execute command
+  $exe $argv
+end
+
+# code (for VSCode) command wrapper for brew-file
+function code
+  # Set exe
+  set -l exe command code
+
+  if type -q brew-file
+    # Check args
+    set -l cmd $argv[1]
+
+    switch "$cmd"
+      case '--install-extension' '--uninstall-extension'
+        set exe brew-file brew code
+    end
+  end
+
+  # Execute command
+  $exe $argv
 end
