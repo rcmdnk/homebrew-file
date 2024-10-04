18bcdb28d0b3bfd04e362ab644f67087c302d025
rcmdnk <rcmdnk@gmail.com>
2024-10-05 02:08:08 +0900

    feat: update to version 9.1.0 in formula url and sha256

--- brew-file.rb
+++ brew-file.rb
@@ -1,8 +1,8 @@
 class BrewFile < Formula
   desc "Brewfile manager for Homebrew."
   homepage "https://github.com/rcmdnk/homebrew-file/"
-  url "https://github.com/rcmdnk/homebrew-file/archive/v9.0.19.tar.gz"
-  sha256 "2294c015248f5c1dfe850e731e109811bc0e5ca485c79c72f95425949f532dce"
+  url "https://github.com/rcmdnk/homebrew-file/archive/v9.1.0.tar.gz"
+  sha256 "858c6723b7204e6df1938a526db8c60e04bdedfe10cfb72ece80a35c86d6acf6"
   license "MIT"
 
   head "https://github.com/rcmdnk/homebrew-file.git"
