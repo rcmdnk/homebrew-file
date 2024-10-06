bfa05b158ef524a6daee714ef9c009912be16c3c
rcmdnk <rcmdnk@gmail.com>
2024-10-07 01:35:26 +0900

    chore: bump Python version requirement to 3.9.0 and update version/date info

--- src/brew_file/info.py
+++ src/brew_file/info.py
@@ -4,14 +4,14 @@ Brew-file: Manager for packages of Homebrew.
 
 https://github.com/rcmdnk/homebrew-file
 
-requirement: Python 3.8.1 or later
+requirement: Python 3.9.0 or later
 """
 __author__ = "rcmdnk"
 __copyright__ = "Copyright (c) 2013 rcmdnk"
 __credits__ = ["rcmdnk"]
 __license__ = "MIT"
-__version__ = "9.1.0"
-__date__ = "5/Oct/2024"
+__version__ = "9.1.2"
+__date__ = "7/Oct/2024"
 __maintainer__ = "rcmdnk"
 __email__ = "rcmdnk@gmail.com"
 __status__ = "Prototype"
