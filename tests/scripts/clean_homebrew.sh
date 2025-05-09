0cab9ca919fde3466b0065e11df146e372061f0e
rcmdnk <rcmdnk@gmail.com>
2025-05-09 22:38:37 +0900

    fix: uninstall only formulae and casks in clean up scripts

--- tests/scripts/clean_homebrew.sh
+++ tests/scripts/clean_homebrew.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-brew list | xargs brew uninstall --zap --force
+brew list --formula | xargs brew uninstall --formula --zap --force
 brew list --cask | xargs brew uninstall --cask --zap --force
 brew tap | xargs brew untap
 
