b7cdf46c571e9abc9ac05b45ee72c0b550d2b102
rcmdnk <rcmdnk@gmail.com>
2025-05-09 22:41:50 +0900

    fix: remove --zap option from brew uninstall in action and test script

--- tests/scripts/clean_homebrew.sh
+++ tests/scripts/clean_homebrew.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-brew list --formula | xargs brew uninstall --formula --zap --force
+brew list --formula | xargs brew uninstall --formula --force
 brew list --cask | xargs brew uninstall --cask --zap --force
 brew tap | xargs brew untap
 
